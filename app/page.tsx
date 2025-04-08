7cf52aa3b47107031c7e5521ad5f8b28db7dd107
helixedra <greencastle08@gmail.com>
2025-04-08 23:07:24 +0200

    meta og

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,32 @@ import Footer from "@/components/Footer";
 import PasswordGenerator from "@/components/PasswordGenerator";
 import Description from "@/components/Description";
 
+export const metadata = {
+  title: "SnapPass - Password Generator",
+  description: "Generate secure and memorable passwords with SnapPass.",
+  openGraph: {
+    title: "SnapPass - Password Generator",
+    description: "Generate secure and memorable passwords with SnapPass.",
+    images: [
+      {
+        url: "https://snappass.capps.fun/images/preview.jpg",
+      },
+    ],
+    url: "https://snappass.capps.fun",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "SnapPass - Password Generator",
+    description: "Generate secure and memorable passwords with SnapPass.",
+    images: [
+      {
+        url: "https://snappass.capps.fun/images/preview.jpg",
+      },
+    ],
+  },
+};
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between py-8 px-4 lg:px-12 lg:py-18 overflow-x-hidden bg-zinc-900 text-white">
